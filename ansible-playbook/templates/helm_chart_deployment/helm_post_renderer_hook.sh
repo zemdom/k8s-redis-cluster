64d135760d07fba330d15805b77b00d8cf4d36a6
zemdom <zembrzuskadom@gmail.com>
2022-08-11 16:19:03 +0200

    Align playbooks to work with Kubernetes v1.24

--- ansible-playbook/templates/helm_chart_deployment/helm_post_renderer_hook.sh
+++ ansible-playbook/templates/helm_chart_deployment/helm_post_renderer_hook.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # SPDX-License-Identifier: BSD-3-Clause
-# Copyright 2021, Intel Corporation
+# Copyright 2021-2022, Intel Corporation
 
 cat <&0 > $KUSTOMIZE_DIR/all.yml
-kustomize build $KUSTOMIZE_DIR
+kubectl kustomize $KUSTOMIZE_DIR
